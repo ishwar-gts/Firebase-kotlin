592ab5e858d56a51e26dbcdd6a778e2ef9538c23
ishwar-gts <97658771+ishwar-gts@users.noreply.github.com>
2023-04-22 15:11:34 +0530

    firestore and storage implementation and image picker done

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -1,16 +1,52 @@
 package com.example.learningproject
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import com.bumptech.glide.Glide
 
 import com.example.learningproject.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val auth=FirebaseAuth.getInstance()
+    private val storage= FirebaseStorage.getInstance()
+    private val dataBase= FirebaseFirestore.getInstance()
+    private var profileUrl=""
+
+    private val requestPermission=registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        it?.let {
+           if (it){
+               Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+           }
+        }
+    }
+
+    private val fileAccess=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        it.data?.data?.let {
+            val inputStream=this.contentResolver.openInputStream(it)
+            inputStream?.readBytes()?.let {
+                uploadFile(it)
+
+            }
+
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
@@ -19,8 +55,21 @@ class MainActivity : AppCompatActivity() {
         binding.signUpButton.setOnClickListener {
             registerUser()
         }
+        binding.profileView.setOnClickListener {
+            sdkIntOverO(this){
+                val intent=Intent(Intent.ACTION_OPEN_DOCUMENT)
+                intent.addCategory(Intent.CATEGORY_OPENABLE)
+                intent.type="*/*"
+                fileAccess.launch(intent)
+
+            }
+        }
+
+
 
     }
+
+
 
     private fun registerUser(){
         val email=binding.emailEditText.text.toString()
@@ -30,25 +79,84 @@ class MainActivity : AppCompatActivity() {
                 binding.emailEditText.requestFocus()}
             password.isEmpty() || password.length<=6->{
                 binding.passwordEditTest.requestFocus()
-            }else->{
+
+            }profileUrl.isEmpty()->{
+            Toast.makeText(this, "profile image is required", Toast.LENGTH_SHORT).show()
+        }else->{
             binding.progressCircular.visibility= View.VISIBLE
             binding.signUpButton.visibility= View.GONE
 
             auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{
-                Toast.makeText(this, "Complete Task", Toast.LENGTH_SHORT).show()
-                binding.progressCircular.visibility= View.GONE
-                binding.signUpButton.visibility= View.VISIBLE
+
+
             }.addOnCanceledListener {
                 Toast.makeText(this, "Sign Up Canceled", Toast.LENGTH_SHORT).show()
-            }.addOnSuccessListener {
                 binding.progressCircular.visibility= View.GONE
                 binding.signUpButton.visibility= View.VISIBLE
+            }.addOnSuccessListener {
+                uploadUserInfoToFirestore(binding.nameEditText.text.toString(),binding.emailEditText.text.toString(),binding.passwordEditTest.text.toString())
                 Toast.makeText(this, it.user?.email, Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener{
+                Log.d("Failure","failred reason ${it.toString()}")
             }
         }
 
         }
     }
 
+    private fun uploadFile(byteArray: ByteArray){
+        val storageRef=storage.reference
+        storageRef.child("images/${Date().time}").putBytes(byteArray).addOnSuccessListener {
+            it.storage.downloadUrl.addOnSuccessListener {
+                Glide.with(this).load(it).centerCrop().into(binding.profileView)
+                profileUrl=it.toString()
+            }
+
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener{
+            Toast.makeText(this, "failure", Toast.LENGTH_SHORT).show()
+        }.addOnCompleteListener{
+            Toast.makeText(this, "complete", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun  sdkIntOverO(context: Context, call:()->Unit){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            if (ActivityCompat.checkSelfPermission(context,android.Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+                call.invoke()
+            }else{
+                requestPermission.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+    private fun uploadUserInfoToFirestore(name:String,email:String,password:String){
+        val map= mutableMapOf<String,String>()
+        map.put("name",name)
+        map.put("email",email)
+        map.put("password",password)
+        map.put("profileUrl",profileUrl)
+        dataBase.collection("user").add(map).addOnSuccessListener {
+            Toast.makeText(this, "Data Added Successfully", Toast.LENGTH_SHORT).show()
+            binding.progressCircular.visibility= View.GONE
+            binding.signUpButton.visibility= View.VISIBLE
+        }.addOnCompleteListener{
+            Toast.makeText(this, "Task Complete", Toast.LENGTH_SHORT).show()
+            binding.progressCircular.visibility= View.GONE
+            binding.signUpButton.visibility= View.VISIBLE
+        }.addOnFailureListener{
+            Toast.makeText(this, "Task Upload Failed ${it.toString()}", Toast.LENGTH_SHORT).show()
+            Log.d("Failure","failred reason ${it.toString()}")
+            binding.progressCircular.visibility= View.GONE
+            binding.signUpButton.visibility= View.VISIBLE
+        }
+
+
+    }
+
 
 }
+
+
+
+
